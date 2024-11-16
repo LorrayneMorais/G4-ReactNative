37104f32c917f270bf60af1d196805228d50b244
Igor André <131011940+IgorAPSantos@users.noreply.github.com>
2024-11-16 19:28:23 -0300

    Update App.tsx

--- gp4TrabalhoFinal/App.tsx
+++ gp4TrabalhoFinal/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Tarefas } from './src/screens/tarefas';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RootStackParamList } from './src/navigation/types';
+import { Home } from './src/screens/home';
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <Tarefas />
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen options={{headerShown: false}} name='Home' component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  }
 
 
