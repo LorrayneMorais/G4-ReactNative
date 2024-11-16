045bb1b65c8dff91d541ff512f2e6594f783b1e8
Lucas Sarasa <lucasmsarasa@gmail.com>
2024-11-16 15:06:38 -0300

    Subindo tela home

---
+++ gp4TrabalhoFinal/src/screens/home/index.tsx
@@ -0,0 +1,85 @@
+import { StatusBar } from "expo-status-bar";
+import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+import backgroundImag from "../../../assets/background.png";
+import iconEat from '../../../assets/eat.png'
+import iconWater from '../../../assets/water.png'
+import calendarIcon from '.../../../assets/calendario.png'
+import heartIcon from '../../../assets/hearticon.png'
+import tarefaIcon from '../../../assets/tarefas.png'
+import moonIcon from '../../../assets/dormir.png'
+import sunIcon from '../../../assets/sun.png'
+import gameIcon from '../../../assets/gameicon.png'
+// import * as Font from 'expo-font';
+import { useState } from "react";
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  // Função para carregar as fontes
+  // const loadFonts = async () => {
+  //   await Font.loadAsync({
+  //     'Doto': require('./assets/fonts/Doto.ttf'), // Caminho para sua fonte
+  //   });
+  //   setFontsLoaded(true);
+  // };
+
+  // if (!fontsLoaded) {
+  //   loadFonts();
+  //   return null;  // Exibe nada enquanto a fonte está carregando
+  // }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={backgroundImag} style={styles.backgroundImage} resizeMode="cover">
+        <View style={styles.topPag}>
+          <View style={styles.topPagContent}>
+              <Text style={styles.topPagContentText}>
+                Virtual Pet
+              </Text>
+          </View>
+        </View>
+        <View style={styles.topPagBarsFoodAndWater}>
+          <View style={styles.topPagBarsFood}>
+            <View style={styles.topPagBarsFoodBlockOne}></View>
+            <View style={styles.topPagBarsFoodBlockTwo}></View>
+            <View style={styles.topPagBarsFoodBlockThree}></View>
+            <View style={styles.topPagBarsFoodBlockFour}></View>
+          </View>
+          <View style={styles.topPagBarsWater}>
+            <View style={styles.topPagBarsWaterBlockOne}></View>
+            <View style={styles.topPagBarsWaterBlockTwo}></View>
+            <View style={styles.topPagBarsWaterBlockThree}></View>
+            <View style={styles.topPagBarsWaterBlockFour}></View>
+          </View>
+        </View>
+        <View style={styles.date}>
+          <View style={styles.dateContainer}>
+            <Text style={styles.dateContainerText}>29</Text>
+          </View>
+        </View>
+        <View style={styles.containerPet}>
+          <View style={styles.containerPetMain}>
+            <Image source={require('./assets/gifs/normal.gif')} style={styles.gif} resizeMode="contain"/>
+          </View>
+        </View>
+        <View style={styles.iconFoodAndWater}>
+          <TouchableOpacity style={styles.iconFood}>
+            <Image source={iconEat} style={styles.iconFoodImage}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconWater}>
+            <Image source={iconWater} style={styles.iconWaterImage}/>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonsInteract}>
+          <TouchableOpacity style={styles.buttonsInteractOne}><Image source={calendarIcon} style={styles.buttonsInteractIcons}/></TouchableOpacity>
+          <TouchableOpacity style={styles.buttonsInteractOne}><Image source={heartIcon} style={styles.buttonsInteractIcons}/></TouchableOpacity>
+          <TouchableOpacity style={styles.buttonsInteractOne}><Image source={tarefaIcon} style={styles.buttonsInteractIcons}/></TouchableOpacity>
+          <TouchableOpacity style={styles.buttonsInteractOne}><Image source={moonIcon} style={styles.buttonsInteractIconsSecond}/></TouchableOpacity>
+          <TouchableOpacity style={styles.buttonsInteractOne}><Image source={sunIcon} style={styles.buttonsInteractIconsSecond}/></TouchableOpacity>
+          <TouchableOpacity style={styles.buttonsInteractOne}><Image source={gameIcon} style={styles.buttonsInteractIcons}/></TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
---
+++ gp4TrabalhoFinal/src/screens/home/styles.ts
@@ -0,0 +1,216 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+  },
+  topPag: {
+    // borderWidth: 1,
+    width: "100%",
+    height: 85,
+    alignItems: "center",
+    // marginBottom: 5
+  },
+  topPagContent: {
+    borderColor: "red",
+    // borderWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    paddingTop: 10,
+    paddingBottom: 16,
+    paddingHorizontal: 35,
+  },
+  topPagContentText: {
+    fontSize: 40,
+    // fontFamily: 'Doto',
+    fontWeight: 600
+  },
+  topPagBarsFoodAndWater: {
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row",
+    // borderWidth: 1,
+    height: 30,
+  },
+  topPagBarsFood: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    width: 150,
+    borderRadius: 20,
+    justifyContent: "space-between",
+    height: "100%",
+  },
+  topPagBarsFoodBlockOne: {
+    borderRightWidth: 1,
+    borderTopLeftRadius: 110,
+    borderBottomLeftRadius: 110,
+    height: "100%",
+    backgroundColor: "orange",
+    width: "25%",
+  },
+  topPagBarsFoodBlockTwo: {
+    borderLeftWidth: 1,
+    height: "100%",
+    backgroundColor: "orange",
+    width: "25%",
+  },
+  topPagBarsFoodBlockThree: {
+    borderLeftWidth: 1,
+    height: "100%",
+    backgroundColor: "orange",
+    width: "25%",
+  },
+  topPagBarsFoodBlockFour: {
+    borderLeftWidth: 2,
+    borderTopRightRadius: 110,
+    borderBottomRightRadius: 110,
+    height: "100%",
+    backgroundColor: "orange",
+    width: "25%",
+  },
+  topPagBarsWater: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    width: 150,
+    borderRadius: 20,
+    justifyContent: "space-between",
+    height: "100%",
+  },
+  topPagBarsWaterBlockOne: {
+    borderRightWidth: 1,
+    borderTopLeftRadius: 110,
+    borderBottomLeftRadius: 110,
+    height: "100%",
+    backgroundColor: "#1CD4E9",
+    width: "25%",
+  },
+  topPagBarsWaterBlockTwo: {
+    borderLeftWidth: 1,
+    height: "100%",
+    backgroundColor: "#1CD4E9",
+    width: "25%",
+  },
+  topPagBarsWaterBlockThree: {
+    borderLeftWidth: 1,
+    height: "100%",
+    backgroundColor: "#1CD4E9",
+    width: "25%",
+  },
+  topPagBarsWaterBlockFour: {
+    borderLeftWidth: 2,
+    borderTopRightRadius: 110,
+    borderBottomRightRadius: 110,
+    height: "100%",
+    backgroundColor: "#1CD4E9",
+    width: "25%",
+  },
+  date: {
+    width: '100%',
+    // borderWidth: 1,
+    marginTop: 15
+  },
+  dateContainer: {
+    borderWidth: 1,
+    backgroundColor: "white",
+    width: 70,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+    marginLeft: 15
+  },
+  dateContainerText: {
+    fontSize: 40
+  },
+  containerPet: {
+    // borderWidth: 1,
+    height: 280,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerPetMain: {
+    borderWidth: 9,
+    borderRadius: '50%',
+    width: 330,
+    height: 330,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    shadowColor: 'black',  // Cor da sombra
+    shadowOpacity: 1,   // Opacidade da sombra
+    shadowRadius: 22,      // Raio de difusão da sombra
+  },
+  gif: {
+    width: 350,
+    height: 350,
+    resizeMode: "contain"
+  },
+  iconFoodAndWater: {
+    // borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    gap: 170
+  },
+  iconFood: {
+    borderWidth: 1,
+    backgroundColor: 'gray',
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%'
+  },
+  iconFoodImage: {
+    width: 30,
+    height: 30,
+    borderRadius: '50%'
+  },
+  iconWater: {
+    borderWidth: 1,
+    backgroundColor: 'gray',
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%'
+  },
+  iconWaterImage: {
+    width: 30,
+    height: 30,
+    borderRadius: '50%'
+  },
+  buttonsInteract: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 50,
+    // borderWidth: 1,
+    padding: 5
+  },
+  buttonsInteractOne: {
+    borderWidth: 1,
+    backgroundColor: 'gray',
+    width: '30%',
+    borderRadius: '50%',
+    height: 120,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonsInteractIcons: {
+    width: 90,
+    height: 80
+  },
+  buttonsInteractIconsSecond: {
+    width: 80,
+    height: 80
+  }
+});
