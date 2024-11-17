28231a5503cb218e2bac63b131aa6b48b6666cd5
Lucas Sarasa <lucasmsarasa@gmail.com>
2024-11-17 19:06:06 -0300

    Subindo routes Tarefas

--- gp4TrabalhoFinal/App.tsx
+++ gp4TrabalhoFinal/App.tsx
@@ -3,14 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { RootStackParamList } from './src/navigation/types';
 import { Home } from './src/screens/home';
+import { Tarefas } from './src/screens/tarefas';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='Tarefas'>
         <Stack.Screen options={{headerShown: false}} name='Home' component={Home}/>
+        <Stack.Screen options={{headerShown: false}} name='Tarefas' component={Tarefas}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
