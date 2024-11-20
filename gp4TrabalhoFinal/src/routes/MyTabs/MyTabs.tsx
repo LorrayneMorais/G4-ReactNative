8520a024d1563eb7c223a9bad1e8dc87a451533e
Lucas Sarasa <lucasmsarasa@gmail.com>
2024-11-20 12:28:56 -0300

    Implementação MyTabs

---
+++ gp4TrabalhoFinal/src/routes/MyTabs/MyTabs.tsx
@@ -0,0 +1,49 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home } from '../../screens/home';
+import { Tarefas } from '../../screens/tarefas';
+import { Image } from 'react-native';
+import { styles } from './styles'
+import homeIconTabs from '../../../assets/casa.png'
+import tarefasIconTabs from '../../../assets/iconTarefasTab.png'
+
+const Tab = createBottomTabNavigator();
+
+export function MyTabs() {
+  return (
+    <Tab.Navigator initialRouteName='Home' 
+    screenOptions={{ 
+      headerShown: false, 
+      tabBarStyle: { 
+        backgroundColor: 'black', 
+        justifyContent: 'center',
+        height: 60
+      }
+    }}>
+      <Tab.Screen
+      options={{
+        tabBarIcon: () => {
+          return <Image 
+          source={homeIconTabs}
+          resizeMode='contain'
+          style={styles.iconsTabs}
+          />
+        }
+      }}  
+      name="Home" 
+      component={Home} 
+      />
+      <Tab.Screen 
+      options={{
+        tabBarIcon: () => {
+          return <Image
+          source={tarefasIconTabs}
+          resizeMode='contain'
+          style={styles.iconsTabs}
+          />
+        }
+      }}
+      name="Tarefas" 
+      component={Tarefas} />
+    </Tab.Navigator>
+  );
+}
---
+++ gp4TrabalhoFinal/src/routes/MyTabs/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    iconsTabs: {
+        width: 20,
+        height: 20,
+        tintColor: 'white'
+    }
+})
